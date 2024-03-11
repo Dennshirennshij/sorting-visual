c27d2df50176945050252817c937468fafc58448
nodedev74 <buelenard@tutanota.com>
2024-03-11 13:21:48 +0100

    build fail

---
+++ src/main/java/github/dennshirennshij/nodedev74/sorting_visual/App.java
@@ -0,0 +1,12 @@
+package github.dennshirennshij.nodedev74.sorting_visual;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
