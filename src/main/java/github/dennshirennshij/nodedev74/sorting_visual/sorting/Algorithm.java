bb42b1a82da8490417304c6f4a95bc85621cb1de
Dennshirennshij <111683708+Dennshirennshij@users.noreply.github.com>
2024-03-13 11:27:06 +0100

    Added Generic Algorithm class, Added AlgorithmWindowInterface

---
+++ src/main/java/github/dennshirennshij/nodedev74/sorting_visual/sorting/Algorithm.java
@@ -0,0 +1,43 @@
+package github.dennshirennshij.nodedev74.sorting_visual.sorting;
+
+import java.util.ArrayList;
+
+public abstract class Algorithm {
+    private AlgorithmWindowInterface window;
+
+    private ArrayList<VisualList> visualLists;
+
+    public abstract void start(int[] elements);
+
+    public Algorithm (AlgorithmWindowInterface window) {
+        this.window = window;
+        this.visualLists = new ArrayList<VisualList>();
+    }
+
+    protected int addVisualList () {
+        VisualList visualList = new VisualList();
+        window.addVisualList(visualList);
+        this.visualLists.add(visualList);
+        return this.visualLists.size() - 1;
+    }
+
+    // todo: add visualList.length equivalent
+
+    protected void trade (int listIndex, int i1, int i2) {
+        window.trade(listIndex, i1, i2);
+        this.visualLists.get(listIndex).trade(i1, i2);
+    }
+
+    protected void set (int listIndex, int index, int value) {
+        window.set(listIndex, index, value);
+        this.visualLists.get(listIndex).set(index, value);
+    }
+
+    protected int get (int listIndex, int index) {
+        window.get(listIndex, index);
+        return this.visualLists.get(listIndex).get(index);
+    }
+
+    // todo: add split operation?
+
+}
