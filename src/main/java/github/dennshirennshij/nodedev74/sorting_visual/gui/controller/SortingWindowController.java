91f2ee380ca7ff5fd237eea2c7dd4acf14643093
nodedev74 <buelenard@tutanota.com>
2024-03-13 16:16:54 +0100

    javafx fxml support added

---
+++ src/main/java/github/dennshirennshij/nodedev74/sorting_visual/gui/controller/SortingWindowController.java
@@ -0,0 +1,2 @@
+package github.dennshirennshij.nodedev74.sorting_visual.gui.controller;public class SortingWindowController {
+}
---
+++ src/main/java/github/dennshirennshij/nodedev74/sorting_visual/gui/element/SortingWindow.java
@@ -0,0 +1,2 @@
+package github.dennshirennshij.nodedev74.sorting_visual.gui.element;public class SortingWindow {
+}
