a6702f9755720f6fb364bcdd292ce8a9a5dc9c61
nodedev74 <buelenard@tutanota.com>
2024-03-13 16:51:34 +0100

    impl window states

--- src/main/java/github/dennshirennshij/nodedev74/sorting_visual/gui/element/SortingWindow.java
+++ src/main/java/github/dennshirennshij/nodedev74/sorting_visual/gui/element/SortingWindow.java
@@ -3,4 +3,21 @@ package github.dennshirennshij.nodedev74.sorting_visual.gui.element;
 import javafx.scene.layout.BorderPane;
 
 public class SortingWindow extends BorderPane {
+
+    public enum WindowState
+    {
+        PAUSED,
+        RUNNING,
+        STOPPED
+    }
+
+    private WindowState currentWindowState;
+
+    public WindowState getCurrentWindowState() {
+        return currentWindowState;
+    }
+
+    public void setCurrentWindowState(WindowState newState) {
+        currentWindowState = newState;
+    }
 }
