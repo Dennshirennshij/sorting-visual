5271b3169b2acc05accc5409b3f539bc02f08fa7
nodedev74 <buelenard@tutanota.com>
2024-03-18 18:07:59 +0100

    added test

--- src/test/java/MainWindowTest.java
+++ src/test/java/MainWindowTest.java
@@ -1,11 +1,15 @@
 import github.dennshirennshij.nodedev74.sorting_visual.App;
 
+import github.dennshirennshij.nodedev74.sorting_visual.sorting.AlgorithmLoader;
 import javafx.application.Application;
+
+import org.junit.jupiter.api.Test;
 
 public class MainWindowTest {
 
     @Test
     void run() {
+        AlgorithmLoader.getInstance().registerClass(BubblesortTest.class);
         Application.launch(App.class);
     }
 }
