d53ae2e6498842030446b83458a63fa8a1485d2d
Dennshirennshij <111683708+Dennshirennshij@users.noreply.github.com>
2024-03-18 17:21:32 +0100

    Added BlueJ example, excluded compiled .class files in gitignore

---
+++ example/Main.java
@@ -0,0 +1,16 @@
+import github.dennshirennshij.nodedev74.sorting_visual.App;
+import javafx.application.Application;
+import github.dennshirennshij.nodedev74.sorting_visual.sorting.AlgorithmLoader;
+
+public class Main
+{
+    public static void main(String[] args) {
+        // Register any custom sorting algorithms
+        AlgorithmLoader.getInstance().registerClass(ExampleAlgorithm.class);
+        
+        // Launch the application
+        // do not change this
+        // do not put anything after this statement
+        Application.launch(App.class, args);
+    }
+}
