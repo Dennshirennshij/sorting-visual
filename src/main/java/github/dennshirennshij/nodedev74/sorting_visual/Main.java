b5bb93715b54b8bec7a397a764096019068be395
Dennshirennshij <111683708+Dennshirennshij@users.noreply.github.com>
2024-03-18 17:34:36 +0100

    Removed Main

--- src/main/java/github/dennshirennshij/nodedev74/sorting_visual/Main.java
+++
@@ -1,9 +0,0 @@
-package github.dennshirennshij.nodedev74.sorting_visual;
-
-import javafx.application.Application;
-
-public class Main {
-    public static void main(String[] args) {
-        Application.launch(App.class, args);
-    }
-}
