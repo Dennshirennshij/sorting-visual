95c63d8f7f76e02c686dba0d6e6b854fa754c58f
Dennshirennshij <111683708+Dennshirennshij@users.noreply.github.com>
2024-03-18 15:26:25 +0100

    Updated README.md, Changed loading custom algorithms

--- README.md
+++ README.md
@@ -7,3 +7,10 @@
  - Fix window sizing issue
  - Fix Algorithm constructor overengineering
  - Impl InputHandler
+
+## Loading of custom Algorithms
+ - This project is a libary
+ - Add it to BlueJ or any other IDE as a dependency
+ - Make your CustomAlgorithms extend our Algorithm class
+ - using out AlgorithmLoader.registerAlgorithm method to register all of your custom Algorithms
+ - Execute our main method in your main method
--- src/main/java/github/dennshirennshij/nodedev74/sorting_visual/sorting/AlgorithmLoader.java
+++ src/main/java/github/dennshirennshij/nodedev74/sorting_visual/sorting/AlgorithmLoader.java
@@ -1,10 +1,6 @@
 package github.dennshirennshij.nodedev74.sorting_visual.sorting;
 
-import java.nio.file.*;
 import java.util.HashMap;
-import java.util.concurrent.TimeUnit;
-
-import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
 
 public class AlgorithmLoader {
 
@@ -13,39 +9,7 @@ public class AlgorithmLoader {
     private HashMap<String, Class<? extends Algorithm>> loadedClasses;
 
     private AlgorithmLoader() {
-
         loadedClasses = new HashMap<>();
-
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Path dir = Paths.get(getClass().getProtectionDomain().getCodeSource().getLocation().toURI()).getParent();
-                    WatchService watchService = FileSystems.getDefault().newWatchService();
-                    dir.register(watchService, ENTRY_CREATE);
-
-                    while (true) {
-                        WatchKey key;
-                        try {
-                            key = watchService.poll(10, TimeUnit.SECONDS); // Warte 10 Sekunden auf ein Event
-                        } catch (InterruptedException e) {
-                            return;
-                        }
-
-                        if (key != null) {
-                            for (WatchEvent<?> event : key.pollEvents()) {
-                                // todo: add classes threadsafe to array
-                            }
-                            key.reset();
-                        }
-                    }
-                }catch (Exception e) {
-                    System.out.println("failed to watch dir");
-                }
-            }
-        });
-
-        thread.start();
     }
 
     public static AlgorithmLoader getInstance() {
@@ -72,42 +36,4 @@ public class AlgorithmLoader {
         return loadedClasses.get(name);
     }
 
-    /*public void load() {
-        // Ordnerpfad zu den JAR-Dateien
-        String pluginsFolderPath = "plugins/";
-
-        // Array für geladene Klassen
-        ArrayList<Class<?>> loadedClasses = new ArrayList<>();
-
-        try {
-            // Verzeichnis für JAR-Dateien erstellen
-            File pluginsFolder = new File(pluginsFolderPath);
-            File[] jarFiles = pluginsFolder.listFiles((dir, name) -> name.endsWith(".jar"));
-
-            if (jarFiles != null) {
-                // URLClassLoader erstellen
-                URLClassLoader classLoader = new URLClassLoader(
-                        new URL[]{pluginsFolder.toURI().toURL()}
-                );
-
-                // Durch alle JAR-Dateien iterieren
-                for (File jarFile : jarFiles) {
-                    // Klasse laden
-                    Class<?> loadedClass = classLoader.loadClass("AlgorithmMain");
-
-                    // Überprüfen, ob die geladene Klasse Algorithm erweitert
-                    if (Algorithm.class.isAssignableFrom(loadedClass)) {
-                        loadedClasses.add(loadedClass);
-                        System.out.println("Klasse geladen: " + loadedClass.getName());
-                    } else {
-                        System.out.println("Klasse in " + jarFile.getName() + " erweitert nicht Algorithm.");
-                    }
-                }
-            } else {
-                System.out.println("Keine JAR-Dateien im Ordner " + pluginsFolderPath + " gefunden.");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }*/
 }
