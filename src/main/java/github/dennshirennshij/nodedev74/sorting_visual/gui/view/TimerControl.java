587fbf4eba96428a5fb3240c231389c732f75552
nodedev74 <buelenard@tutanota.com>
2024-03-21 20:36:48 +0100

    removed warns

--- src/main/java/github/dennshirennshij/nodedev74/sorting_visual/gui/view/TimerControl.java
+++ src/main/java/github/dennshirennshij/nodedev74/sorting_visual/gui/view/TimerControl.java
@@ -3,15 +3,13 @@ package github.dennshirennshij.nodedev74.sorting_visual.gui.view;
 import javafx.application.Platform;
 import javafx.scene.control.Label;
 
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class TimerControl extends Label {
 
-    private Timer timer;
+    private final Timer timer;
 
     private final TimerTask timerTask;
     private boolean isPaused;
